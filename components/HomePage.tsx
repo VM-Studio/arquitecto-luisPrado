1a4b6ef4b96abb54a7ada62434c15f93c847c8f0
Valentina <vaalendelatorre@gmail.com>
2026-01-20 22:13:19 -0300

    ultimas modificaciones en la velocidad y diseno

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -1,14 +1,11 @@
 'use client'
 
-import { useScrollAnimation } from '@/hooks/useScrollAnimation'
 import Hero from '@/components/Hero'
 import About from '@/components/About'
 import Services from '@/components/Services'
 import Contact from '@/components/Contact'
 
 export default function HomePage() {
-  useScrollAnimation()
-
   return (
     <main>
       <Hero />
